e10e03b03129793985ed6740d1b4e1a639083d32
Sonali <sonalidhikale0@gmail.com>
2022-12-19 16:30:01 +0530

    SB-2|Created Signup page

---
+++ API/model/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const userCollection = "user";
+const User = mongoose.model(userCollection, {
+  email: String,
+  password: String,
+  authToken: [
+    {
+      type: String,
+    },
+  ],
+});
+
+module.exports = User;
